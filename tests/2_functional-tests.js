e25e2ee463b8cbb6a69c85215ce4a896468b7c5b
101jah <carlsmallzz@outlook.com>
2024-06-01 15:51:49 -0500

    Updated

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,11 +1,3 @@
-/*
-*
-*
-*       FILL IN EACH FUNCTIONAL TEST BELOW COMPLETELY
-*       -----[Keep the tests in the same order!]-----
-*       (if additional are added, keep them at the very end!)
-*/
-
 var chaiHttp = require('chai-http');
 var chai = require('chai');
 var assert = chai.assert;
@@ -14,94 +6,105 @@ var server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-    
+
     suite('GET /api/stock-prices => stockData object', function() {
-      
-      test('1 stock', function(done) {
-       chai.request(server)
-        .get('/api/stock-prices')
-        .query({stock: 'goog'})
-        .end(function(err, res){
-          assert.equal(res.body['stockData']['stock'], 'goog')
-          assert.isNotNull(res.body['stockData']['price'])
-           assert.isNotNull(res.body['stockData']['likes'])
-          done();
+
+        // Test viewing one stock
+        test('Viewing one stock: GET request to /api/stock-prices/', function(done) {
+            chai.request(server)
+            .get('/api/stock-prices')
+            .query({stock: 'GOOG'})
+            .end(function(err, res) {
+                assert.equal(res.status, 200);
+                assert.equal(res.body.stockData.stock, 'GOOG');
+                assert.isNotNull(res.body.stockData.price);
+                assert.isNotNull(res.body.stockData.likes);
+                done();
+            });
         });
-      });
-      
-      test('1 stock with like', function(done) {
-        chai.request(server)
-        .get('/api/stock-prices')
-        .query({stock: 'aapl', like: true})
-        .end(function(err, res){
-          assert.equal(res.body['stockData']['stock'], 'aapl')
-          assert.equal(res.body['stockData']['likes'], 1)
-          done();
+
+        // Test viewing one stock and liking it
+        test('Viewing one stock and liking it: GET request to /api/stock-prices/', function(done) {
+            chai.request(server)
+            .get('/api/stock-prices')
+            .query({stock: 'GOOG', like: true})
+            .end(function(err, res) {
+                assert.equal(res.status, 200);
+                assert.equal(res.body.stockData.stock, 'GOOG');
+                assert.isNotNull(res.body.stockData.price);
+                assert.equal(res.body.stockData.likes, 1);
+                done();
+            });
         });
-      });
-      
-      test('1 stock with like again (ensure likes arent double counted)', function(done) {
-        chai.request(server)
-        .get('/api/stock-prices')
-        .query({stock: 'aapl', like: true})
-        .end(function(err, res){
-          assert.equal(res.body, 'Error: Only 1 Like per IP Allowed')
-          done()
+
+        // Test viewing the same stock and liking it again to ensure likes are not double-counted
+        test('Viewing the same stock and liking it again: GET request to /api/stock-prices/', function(done) {
+            chai.request(server)
+            .get('/api/stock-prices')
+            .query({stock: 'GOOG', like: true})
+            .end(function(err, res) {
+                assert.equal(res.status, 200);
+                assert.equal(res.body, 'Error: Only 1 Like per IP Allowed');
+                done();
+            });
         });
-      });
-      
-      test('2 stocks', function(done) {
-        chai.request(server)
-        .get('/api/stock-prices')
-        .query({stock: ['aapl', 'amzn']})
-        .end(function(err, res){
-          let stockData = res.body['stockData']
-          assert.isArray(stockData)
-          /* Stocks can come in either order */
-          if(stockData[0]['stock'] === 'aapl'){
-            assert.equal(stockData[0]['stock'], 'aapl')
-            assert.equal(stockData[0]['likes'], 1)
-            assert.equal(stockData[0]['rel_likes'], 1)
-            assert.equal(stockData[1]['stock'], 'amzn')
-            assert.equal(stockData[1]['likes'], 0)
-            assert.equal(stockData[1]['rel_likes'], -1)
-          }else{
-            assert.equal(stockData[1]['stock'], 'aapl')
-            assert.equal(stockData[1]['likes'], 1)
-            assert.equal(stockData[1]['rel_likes'], 1)
-            assert.equal(stockData[0]['stock'], 'amzn')
-            assert.equal(stockData[0]['likes'], 0)
-            assert.equal(stockData[0]['rel_likes'], -1)
-          }
-          done()
+
+        // Test viewing two stocks
+        test('Viewing two stocks: GET request to /api/stock-prices/', function(done) {
+            chai.request(server)
+            .get('/api/stock-prices')
+            .query({stock: ['AAPL', 'MSFT']})
+            .end(function(err, res) {
+                let stockData = res.body.stockData;
+                assert.isArray(stockData);
+
+                if(stockData[0].stock === 'AAPL') {
+                    assert.equal(stockData[0].stock, 'AAPL');
+                    assert.isNotNull(stockData[0].price);
+                    assert.isNotNull(stockData[0].rel_likes);
+                    assert.equal(stockData[1].stock, 'MSFT');
+                    assert.isNotNull(stockData[1].price);
+                    assert.isNotNull(stockData[1].rel_likes);
+                } else {
+                    assert.equal(stockData[1].stock, 'AAPL');
+                    assert.isNotNull(stockData[1].price);
+                    assert.isNotNull(stockData[1].rel_likes);
+                    assert.equal(stockData[0].stock, 'MSFT');
+                    assert.isNotNull(stockData[0].price);
+                    assert.isNotNull(stockData[0].rel_likes);
+                }
+                done();
+            });
         });
-      });
-      
-      test('2 stocks with like', function(done) {
-        chai.request(server)
-        .get('/api/stock-prices')
-        .query({stock: ['spot', 'amzn'], like: true})
-        .end(function(err, res){
-          let stockData = res.body.stockData
-          if(stockData[0]['stock'] === 'spot'){
-            assert.equal(stockData[0]['stock'], 'spot')
-            assert.equal(stockData[0]['likes'], 1)
-            assert.equal(stockData[0]['rel_likes'], 0)
-            assert.equal(stockData[1]['stock'], 'amzn')
-            assert.equal(stockData[1]['likes'], 1)
-            assert.equal(stockData[1]['rel_likes'], 0)
-          }else{
-            assert.equal(stockData[1]['stock'], 'spot')
-            assert.equal(stockData[1]['likes'], 1)
-            assert.equal(stockData[1]['rel_likes'], 0)
-            assert.equal(stockData[0]['stock'], 'amzn')
-            assert.equal(stockData[0]['likes'], 1)
-            assert.equal(stockData[0]['rel_likes'], 0)
-          }
-          done()
+
+        // Test viewing two stocks and liking them
+        test('Viewing two stocks and liking them: GET request to /api/stock-prices/', function(done) {
+            chai.request(server)
+            .get('/api/stock-prices')
+            .query({stock: ['AAPL', 'MSFT'], like: true})
+            .end(function(err, res) {
+                let stockData = res.body.stockData;
+                assert.isArray(stockData);
+
+                if(stockData[0].stock === 'AAPL') {
+                    assert.equal(stockData[0].stock, 'AAPL');
+                    assert.isNotNull(stockData[0].price);
+                    assert.isNotNull(stockData[0].rel_likes);
+                    assert.equal(stockData[1].stock, 'MSFT');
+                    assert.isNotNull(stockData[1].price);
+                    assert.isNotNull(stockData[1].rel_likes);
+                } else {
+                    assert.equal(stockData[1].stock, 'AAPL');
+                    assert.isNotNull(stockData[1].price);
+                    assert.isNotNull(stockData[1].rel_likes);
+                    assert.equal(stockData[0].stock, 'MSFT');
+                    assert.isNotNull(stockData[0].price);
+                    assert.isNotNull(stockData[0].rel_likes);
+                }
+                done();
+            });
         });
-      });
-      
+
     });
 
 });
